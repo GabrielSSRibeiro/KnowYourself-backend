400a7186902db0e35fe069681ae5ab270a195d49
Gabriel Ribeiro <49738156+GabrielSSRibeiro@users.noreply.github.com>
2021-07-27 23:31:53 -0400

    added sample db

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+using server.Models;
 
 namespace server.Controllers
 {
@@ -6,9 +10,41 @@ namespace server.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
+
+        private readonly ApiDbContext _context;
+
+        public TodoController(ApiDbContext context){
+            _context = context;
+        }
+
         [HttpGet]
-        public IActionResult TestRun(){
-            return Ok("success");
+        public async Task<IActionResult> GetItems(){
+            var items = await _context.Items.ToListAsync();
+            
+            return Ok(items);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateItem(ItemData data){
+            if(ModelState.IsValid){
+                await _context.Items.AddAsync(data);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetItem", new {data.Id}, data);
+            }
+            
+            return new JsonResult("Something went wrong") {StatusCode = 500};
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id){
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(item == null){
+                return NotFound();
+            }
+
+            return Ok(item);
         }
     }
 }
